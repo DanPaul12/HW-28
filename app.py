364f58032bdce2cc6e51a09dfbdfdc7b84f292c0
DanPaul12 <dan.paul.schechter@gmail.com>
2024-08-28 11:03:44 -0700

    hw28

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, json, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+mysqlconnector://root:thegoblet2@localhost/e_commerce_db'
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Customer(db.Model):
+    __tablename__ = 'Customer'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String (255), nullable = False)
+    email = db.Column(db.String (320))
+    phone = db.Column(db.String (15))
+    orders = db.relationship('Order', backref = 'customer')
+
+customer_schema = Customer()
+custotmers_schema = Customer(many=True)
+
+class Orders(db.Model):
+    __tablename__ = 'Orders'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable = False)
+    customer_id = db.Column(db.Integer, db.ForeignKey("Customer.id"))
+
+class CustomerAcccount(db.Model):
+    __tablename__ = 'CustomerAccount'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable = False)
+    password = db.Column(db.String(255), nullable = False)
+    customer_id = db.Column(db.Integer, db.ForeignKey("Customer.id"))
+    customer = db.relationship('Customer', backref = 'customer_account', uselist=False)
+
+
+order_product = db.Table('Order_Product',
+        db.Column('order_id', db.Integer, db.ForeignKey('Orders.id'), primary_key = True),
+        db.Column('product_id', db.Integer, db.ForeignKey('Products.id'), primary_key = True))
+
+
+class Product(db.Model):
+    __tablename__ = 'Products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String (255), nullable = False)
+    price = db.Column(db.Float, nullable = False)
+
+
+@app.route('/customers', methods= ['POST'])
+def add_customer():
+    customer_data = customer_schema.load(request.json)
+    if customer_data is None:
+        return jsonify({'message':'customer not found'})
+    customer = Customer(name = customer_data['name'], email = customer_data['email'], phone = customer_data['phone'])
+    db.session.add(customer)
+    db.session.commit()
+    return jsonify({'message':'customer added'}), 200
+
+    
